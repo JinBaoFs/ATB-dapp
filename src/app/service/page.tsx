568bdfda58ebffeb09b718da9dfd2fe0d738ec8a
yangjinbao <13631531284@163.com>
2024-07-15 15:17:13 +0800

    修改

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -116,7 +116,7 @@ export default function Service() {
                 receiveUSDTConfirm()
             }else{
                 receiveATBConfirm()
-            }    
+            }            
         }
     },[LPIssuccess])
 
